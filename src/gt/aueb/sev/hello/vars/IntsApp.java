c44715f1ede843f060d91a1732337f98f6534cd3
TheoKon <teogamer2019@gmail.com>
2022-09-27 19:09:51 +0300

    ins v2.0

--- src/gt/aueb/sev/hello/vars/IntsApp.java
+++ src/gt/aueb/sev/hello/vars/IntsApp.java
@@ -3,7 +3,7 @@ package gt.aueb.sev.hello.vars;
 public class IntsApp {
     public static void main(String[] args) {
 
-        int num1 = 10;
+        int num1 = 300;
         int num2 = 20;
         int result = num2  + num1;
         System.out.println("TO APOTELESMA TOU :"+num1+" KAI TOU "+num2+" EINAI : "+result);
