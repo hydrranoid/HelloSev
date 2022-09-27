da82adc6bbb69fa652b0f78628f1cd4d629057f5
TheoKon <teogamer2019@gmail.com>
2022-09-27 18:42:04 +0300

    Hello Package

---
+++ src/gt/aueb/sev/hello/hello/HelloSev.java
@@ -0,0 +1,7 @@
+package gt.aueb.sev.hello.hello;
+
+public class HelloSev {
+    public static void main(String[] args) {
+        System.out.println("HELLO SEB !");
+    }
+}
---
+++ src/gt/aueb/sev/hello/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package gt.aueb.sev.hello.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HELLO world !");
+    }
+}
